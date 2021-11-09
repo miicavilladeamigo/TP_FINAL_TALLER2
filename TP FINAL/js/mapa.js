3b07be330cd8fe22a09f585e300493c7a200a424
lucagalottov <66344789+lucagalottov@users.noreply.github.com>
2021-11-09 18:00:31 -0300

    Add files via upload

---
+++ TP FINAL/js/mapa.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+
+      $("#hEeuu").hover(mouseEnter, mouseLeave);
+      function mouseEnter() {
+           $("#eeuu").fadeIn( "slow" ).show();
+      };
+      function mouseLeave() {
+            $("#eeuu").fadeOut( "slow" );
+      };
+
+})
