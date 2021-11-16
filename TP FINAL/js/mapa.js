38dcd9485f3c42dab4754c04ea12c0ce5ee2674f
lucagalottov <66344789+lucagalottov@users.noreply.github.com>
2021-11-16 01:49:23 -0300

    Add files via upload

--- TP FINAL/js/mapa.js
+++ TP FINAL/js/mapa.js
@@ -1,11 +1,102 @@
 $(document).ready(function () {
 
-      $("#hEeuu").hover(mouseEnter, mouseLeave);
+      /* ESTADOS UNIDOS */
+      $(".usa").hover(mouseEnter, mouseLeave);
       function mouseEnter() {
-           $("#eeuu").fadeIn( "slow" ).show();
+           $("#eeuu").fadeIn( "fast" ).show();
       };
       function mouseLeave() {
-            $("#eeuu").fadeOut( "slow" );
+            $("#eeuu").fadeOut( "fast" );
+      };
+
+      /* BRASIL */
+      $("#BR").hover(mouseEnter2, mouseLeave2);
+      function mouseEnter2() {
+           $("#brasil").fadeIn( "fast" ).show();
+      };
+      function mouseLeave2() {
+            $("#brasil").fadeOut( "fast" );
+      };
+
+      /* ARGENTINA */
+      $(".Argentina").hover(mouseEnter3, mouseLeave3);
+      function mouseEnter3() {
+           $("#argentina").fadeIn( "fast" ).show();
+      };
+      function mouseLeave3() {
+            $("#argentina").fadeOut( "fast" );
+      };
+
+      /* ESPAÑA */
+      $("#ES").hover(mouseEnter4, mouseLeave4);
+      function mouseEnter4() {
+           $("#espana").fadeIn( "fast" ).show();
+      };
+      function mouseLeave4() {
+            $("#espana").fadeOut( "fast" );
+      };
+
+      /* IRLANDA */
+      $("#IE").hover(mouseEnter5, mouseLeave5);
+      function mouseEnter5() {
+           $("#irlanda").fadeIn( "fast" ).show();
+      };
+      function mouseLeave5() {
+            $("#irlanda").fadeOut( "fast" );
+      };
+
+      /* INGLATERRA */
+      $(".UnitedKingdom").hover(mouseEnter6, mouseLeave6);
+      function mouseEnter6() {
+           $("#inglaterra").fadeIn( "fast" ).show();
+      };
+      function mouseLeave6() {
+            $("#inglaterra").fadeOut( "fast" );
+      };
+
+      /* Suiza */
+      $("#CH").hover(mouseEnter7, mouseLeave7);
+      function mouseEnter7() {
+           $("#suiza").fadeIn( "fast" ).show();
+      };
+      function mouseLeave7() {
+            $("#suiza").fadeOut( "fast" );
+      };
+
+      /* Alemania */
+      $("#DE").hover(mouseEnter8, mouseLeave8);
+      function mouseEnter8() {
+           $("#alemania").fadeIn( "fast" ).show();
+      };
+      function mouseLeave8() {
+            $("#alemania").fadeOut( "fast" );
+      };
+
+      /* Checa */
+      $("#CZ").hover(mouseEnter9, mouseLeave9);
+      function mouseEnter9() {
+           $("#checa").fadeIn( "fast" ).show();
+      };
+      function mouseLeave9() {
+            $("#checa").fadeOut( "fast" );
+      };
+
+      /* japon */
+      $(".Japan").hover(mouseEnter10, mouseLeave10);
+      function mouseEnter10() {
+           $("#japon").fadeIn( "fast" ).show();
+      };
+      function mouseLeave10() {
+            $("#japon").fadeOut( "fast" );
+      };
+
+      /* india */
+      $("#IN").hover(mouseEnter11, mouseLeave11);
+      function mouseEnter11() {
+           $("#india").fadeIn( "fast" ).show();
+      };
+      function mouseLeave11() {
+            $("#india").fadeOut( "fast" );
       };
 
 })
