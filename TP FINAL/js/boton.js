e288fd79721a39dd430e9bd290b5a030aa6c6502
miicavilladeamigo <66319880+miicavilladeamigo@users.noreply.github.com>
2021-11-30 20:27:47 -0300

    Add files via upload

--- TP FINAL/js/boton.js
+++ TP FINAL/js/boton.js
@@ -1,13 +1,29 @@
+//---- SLIDER----//
+
+//var slider = document.getElementById("myRange");
+//var output = document.getElementById("demo");
+//var output2 = document.getElementById("demo2");
+//var output3 = document.getElementById("demo3");
+//output.innerHTML = slider.value;
+//output2.innerHTML = slider.value;
+//output3.innerHTML = slider.value;
+
+//slider.oninput = function() {
+  //output.innerHTML = this.value;
+  //output2.innerHTML = this.value;
+  //output3.innerHTML = this.value;
+//}
 var slider = document.getElementById("myRange");
 var output = document.getElementById("demo");
-var output2 = document.getElementById("demo2");
-var output3 = document.getElementById("demo3");
+var output2 = document.getElementById ("demo2");
+var output3 = document.getElementById ("demo3");
+
 output.innerHTML = slider.value;
-output2.innerHTML = slider.value;
-output3.innerHTML = slider.value;
+output2.innterHTML = slider.value * 15500;
+output3.innterHTML = slider.value * 8;
 
 slider.oninput = function() {
   output.innerHTML = this.value;
-  output2.innerHTML = this.value;
-  output3.innerHTML = this.value;
+  output2.innerHTML = this.value * 15500;
+  output3.innerHTML = this.value * 8;
 }
