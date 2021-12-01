67c2b6883adfca90d1cbcc2ce564e69f94deec2e
miicavilladeamigo <66319880+miicavilladeamigo@users.noreply.github.com>
2021-12-01 16:23:29 -0300

    Add files via upload

--- TP FINAL/js/boton.js
+++ TP FINAL/js/boton.js
@@ -1,21 +1,10 @@
-//---- SLIDER----//
-
-//var slider = document.getElementById("myRange");
-//var output = document.getElementById("demo");
-//var output2 = document.getElementById("demo2");
-//var output3 = document.getElementById("demo3");
-//output.innerHTML = slider.value;
-//output2.innerHTML = slider.value;
-//output3.innerHTML = slider.value;
-
-//slider.oninput = function() {
-  //output.innerHTML = this.value;
-  //output2.innerHTML = this.value;
-  //output3.innerHTML = this.value;
-//}
+//-----SLIDER----//
 var slider = document.getElementById("myRange");
+//---KILOS DE CARNE---//
 var output = document.getElementById("demo");
+//---LITROS DE AGUA---//
 var output2 = document.getElementById ("demo2");
+//----PERSONAS---//
 var output3 = document.getElementById ("demo3");
 
 output.innerHTML = slider.value;
