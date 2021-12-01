487ad0f8d16d554569560b6740cf4e27237084e0
miicavilladeamigo <66319880+miicavilladeamigo@users.noreply.github.com>
2021-12-01 16:34:40 -0300

    Add files via upload

--- TP FINAL/js/boton.js
+++ TP FINAL/js/boton.js
@@ -7,6 +7,7 @@ var output2 = document.getElementById ("demo2");
 //----PERSONAS---//
 var output3 = document.getElementById ("demo3");
 
+
 output.innerHTML = slider.value;
 output2.innterHTML = slider.value * 15500;
 output3.innterHTML = slider.value * 8;
@@ -15,4 +16,5 @@ slider.oninput = function() {
   output.innerHTML = this.value;
   output2.innerHTML = this.value * 15500;
   output3.innerHTML = this.value * 8;
+  oculto.slideDown;
 }
